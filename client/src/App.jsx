0da87191583eee7ccc14f2cf843699a8dd39cec7
fruitxxsnackz <153016953+fruitxxsnackz@users.noreply.github.com>
2024-06-03 18:43:10 -0700

    Resolving duplicate error (#15)
    * Updating schema, utils, and models under server side.
    
    * Updating server.js, seed.js, and db files, still editing.
    
    * Adding Workout folders under clients/src/components, editing server.js.
    
    * Adding Workout Folders.
    
    * Pushing updated index files under ExerciseList/WorkoutList.
    
    * Delete duplicate
    
    * Resolving duplicate error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,5 @@
-
-
-
-// export default function App() {
-//   return (
-//       <h1>App</h1>
-//   );
-// }
-
-import React from 'react';
-
-const ExerciseList = ({ exercises = [] }) => {
-  if (!exercises.length) {
-    return <h3>No Exercises Yet</h3>;
-  }
-
+ export default function App() {
   return (
-    <>
-      <h3
-        className="p-5 display-inline-block"
-        style={{ borderBottom: '1px dotted #1a1a1a' }}
-      >
-        Exercises
-      </h3>
-      <div className="flex-row my-4">
-        {exercises &&
-          exercises.map((exercise) => (
-            <div key={exercise._id} className="col-12 mb-3 pb-3">
-              <div className="p-3 bg-dark text-light">
-                <h5 className="card-header">
-                  {exercise.name}
-                </h5>
-                <p className="card-body">
-                  Sets: {exercise.sets} <br />
-                  Reps: {exercise.reps} <br />
-                  {exercise.weight && `Weight: ${exercise.weight} lbs`}
-                </p>
-              </div>
-            </div>
-          ))}
-      </div>
-    </>
-  );
-};
-
-export default ExerciseList;
+      <h1>App</h1>
+ );
+ }
